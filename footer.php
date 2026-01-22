08edac1d94c2e926b9546cd9f8a074d29d790076
aelhatria-esliceu <aelhatria@esliceu.net>
2026-01-22 15:48:21 +0100

    Adiciones en footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <footer>
     <h3>Aquí ira nuestro pie de página</h3>
 </footer>
-
+<?php wp_footer(); ?>
 </body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </head>
