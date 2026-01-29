5c83e6766aa19cec77957489bcc96dbc2835a82e
aelhatria-esliceu <aelhatria@esliceu.net>
2026-01-29 15:48:43 +0100

    Solucionar despliegue menu Boostrap

--- header.php
+++ header.php
@@ -9,7 +9,13 @@
 <body <?php body_class(); ?>>
     <div class="cabecera">
         <nav class="navbar navbar-expand-md navbar-light bg-light">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <?php
@@ -18,7 +24,7 @@
                 "depth"           => 2, // 1 = no dropdowns, 2 = with dropdowns.
                 "container"       => "div",
                 "container_class" => "collapse navbar-collapse",
-                "container_id"    => "bs-example-navbar-collapse-1",
+                "container_id"    => "navbarNav",
                 "menu_class"      => "navbar-nav mr-auto",
                 "fallback_cb"     => "WP_Bootstrap_Navwalker::fallback",
                 "walker"          => new WP_Bootstrap_Navwalker(),
