99e0962ff78372a940b4674f08cb0ce98ba74738
aelhatria-esliceu <aelhatria@esliceu.net>
2026-02-05 16:39:42 +0100

    Editar páginas con el editor de bloques de Wordpress

--- page.php
+++ page.php
@@ -1,7 +1,15 @@
 <?php
     get_header();
 ?>
-    page.php
+    <?php
+        while(have_posts()){
+            the_post();
+            ?>
+            <h2><a href="<?=the_permalink()?>"><?=the_title()?></a></h2>
+            <p><?=the_content()?></p>
+        <?php
+        }
+    ?>
 <?php
     get_footer();
 ?>
