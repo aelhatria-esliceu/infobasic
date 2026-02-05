3ede4c2b913f6f80fd1dacf74d5a859af07e6db9
aelhatria-esliceu <aelhatria@esliceu.net>
2026-02-05 16:52:16 +0100

    Crear page templates para cada una de las páginas de nuestra tienda

---
+++ page-camisetas.php
@@ -0,0 +1,9 @@
+<?php
+    get_header();
+?>
+    <div class="jumbotron jumbo-tienda">
+        <h1>Compra nuestras camisetas</h1>
+    </div>
+<?php
+    get_footer();
+?>
---
+++ page-tejanos.php
@@ -0,0 +1,9 @@
+<?php
+    get_header();
+?>
+    <div class="jumbotron jumbo-jeans">
+        <h1>Compra nuestros jeans flexibles</h1>
+    </div>
+<?php
+    get_footer();
+?>
