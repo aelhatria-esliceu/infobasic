9c32928e054661ac50832c6ca5c85472456db23c
aelhatria-esliceu <aelhatria@esliceu.net>
2026-02-08 11:51:50 +0100

    Personalizar page tejanos php

--- page-tejanos.php
+++ page-tejanos.php
@@ -4,6 +4,34 @@
     <div class="jumbotron jumbo-jeans">
         <h1>Compra nuestros jeans flexibles</h1>
     </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card-img-top">
+                    <img src="<?= get_theme_file_uri("inc/img/Imagen (11).jpg") ?>" alt="" srcset="">
+                    <p>1981 - 19,90 €</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card-img-top">
+                    <img src="<?= get_theme_file_uri("inc/img/Imagen (11).jpg") ?>" alt="" srcset="">
+                    <p>1981 - 19,90 €</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card-img-top">
+                    <img src="<?= get_theme_file_uri("inc/img/Imagen (11).jpg") ?>" alt="" srcset="">
+                    <p>1981 - 19,90 €</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card-img-top">
+                    <img src="<?= get_theme_file_uri("inc/img/Imagen (11).jpg") ?>" alt="" srcset="">
+                    <p>1981 - 19,90 €</p>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php
     get_footer();
 ?>
